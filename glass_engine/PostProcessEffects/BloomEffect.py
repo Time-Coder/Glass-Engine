40a7d940f4070334c9ae63f2cb1e9db66ebe2254
Time-Coder <binghuiwang5@gmail.com>
2023-09-12 17:15:55 +0800

    add user properties to BloomEffect

--- glass_engine/PostProcessEffects/BloomEffect.py
+++ glass_engine/PostProcessEffects/BloomEffect.py
@@ -13,17 +13,19 @@ class BloomEffect(PostProcessEffect):
     def __init__(self):
         PostProcessEffect.__init__(self)
         
-        self.last_src_width = 0
-        self.last_src_height = 0
+        self._src_width = 0
+        self._src_height = 0
 
-        self.bloom_down_fbo_list = []
-        self.used_length = 0
-
+        self._bloom_down_fbo_list = []
+        
         self._down_program = None
         self._up_program = None
         self._mix_fbo = None
         self._mix_program = None
-        self._threshold = 1
+
+        self.strength:float = 0.5
+        self.threshold:float = 1
+        self.times:int = 6
 
     @property
     def down_program(self):
@@ -60,12 +62,22 @@ class BloomEffect(PostProcessEffect):
         
         return self._mix_program
     
-    def __get_bloom_image(self, screen_image):
+    def bloom_down_fbo(self, i:int)->FBO:
+        if i < len(self._bloom_down_fbo_list):
+            return self._bloom_down_fbo_list[i]
+        else:
+            return None
+
+    def __get_bloom_image(self, screen_image:sampler2D):
         self.__update_fbo_list(screen_image.width, screen_image.height)
         
         with GLConfig.LocalConfig(cull_face=None, polygon_mode=GL.GL_FILL):
-            for i in range(self.used_length):
-                down_fbo = self.bloom_down_fbo_list[i]
+            current_index = 0
+            for i in range(self._times):
+                down_fbo = self.bloom_down_fbo(i)
+                if down_fbo is None:
+                    break
+
                 with down_fbo:
                     GLConfig.clear_buffers()
                     self.down_program["threshold"] = self._threshold
@@ -73,14 +85,15 @@ class BloomEffect(PostProcessEffect):
                     self.down_program["screen_image"] = screen_image
                     self.down_program.draw_triangles(Frame.vertices, Frame.indices)
                 screen_image = down_fbo.color_attachment(0)
+                current_index = i
 
             with GLConfig.LocalConfig(blend=True,
                 blend_src_rgb=GL.GL_ONE, blend_dest_rgb=GL.GL_ONE,
                 blend_src_alpha=GL.GL_ONE, blend_dest_alpha=GL.GL_ONE):
-                for i in range(self.used_length-1, 0, -1):
-                    down_fbo = self.bloom_down_fbo_list[i]
+                for i in range(current_index, -1, -1):
+                    down_fbo = self.bloom_down_fbo(i)
                     with down_fbo:
-                        self.up_program["filter_radius"] = down_fbo.width/10000
+                        self.up_program["filter_radius"] = 0.02 * self._strength
                         self.up_program["screen_image"] = screen_image
                         self.up_program.draw_triangles(Frame.vertices, Frame.indices)
                     screen_image = down_fbo.color_attachment(0)
@@ -113,11 +126,11 @@ class BloomEffect(PostProcessEffect):
         self._threshold = threshold
 
     def __update_fbo_list(self, src_width:int, src_height:int):
-        if src_width == self.last_src_width and \
-           src_height == self.last_src_height:
+        if src_width == self._src_width and \
+           src_height == self._src_height:
             return
         
-        dest_width = 16
+        dest_width = 8
         dest_height = int(dest_width / src_width * src_height)
 
         width_list, height_list = [], []
@@ -146,23 +159,23 @@ class BloomEffect(PostProcessEffect):
             height_list.extend([height_list[-1]]*(-delta_len))
             len_height_list -= delta_len
 
-        len_bloom_down_fbo_list = len(self.bloom_down_fbo_list)
+        len_bloom_down_fbo_list = len(self._bloom_down_fbo_list)
         while len_bloom_down_fbo_list < len_width_list:
             fbo = FBO()
             fbo.auto_clear = False
             fbo.attach(0, sampler2D)
-            self.bloom_down_fbo_list.append(fbo)
+            self._bloom_down_fbo_list.append(fbo)
             len_bloom_down_fbo_list += 1
 
         self.used_length = len_width_list
 
         for i in range(len_width_list):
-            bloom_down_fbo = self.bloom_down_fbo_list[i]
+            bloom_down_fbo = self._bloom_down_fbo_list[i]
 
             width = width_list[i]
             height = height_list[i]
 
             bloom_down_fbo.resize(width, height)
 
-        self.last_src_width = src_width
-        self.last_src_height = src_height
+        self._src_width = src_width
+        self._src_height = src_height
