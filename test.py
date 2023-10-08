85e0cf1de149fdbc51f309b49cc8a00158aaa499
Time-Coder <binghuiwang5@gmail.com>
2023-10-08 10:36:17 +0800

    remove test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-from glass.utils import pip_install
-
-os.environ["INCLUDE"] = "G:/Python-workspace/glass_engine/glass_engine/OpenEXR_dist/include/OpenEXR;G:/Python-workspace/glass_engine/glass_engine/OpenEXR_dist/include/Imath;" + os.environ["INCLUDE"]
-os.environ["LIB"] = "G:/Python-workspace/glass_engine/glass_engine/OpenEXR_dist/lib;" + os.environ["LIB"]
-os.environ["PATH"] = "G:/Python-workspace/glass_engine/glass_engine/OpenEXR_dist/bin;" + os.environ["PATH"]
-pip_install("OpenEXR")
