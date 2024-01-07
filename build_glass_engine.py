e806dc011b8b99712570a117b69e4a62cb274f72
Time-Coder <binghuiwang5@gmail.com>
2024-01-07 18:42:10 +0800

    rename files

--- build_glass_engine.py
+++ build_glass_engine.py
@@ -6,16 +6,16 @@ import shutil
 if os.path.isdir("build"):
     shutil.rmtree("build")
 
-if os.path.isdir("python_glass.egg-info"):
-    shutil.rmtree("python_glass.egg-info")
+if os.path.isdir("glass_engine.egg-info"):
+    shutil.rmtree("glass_engine.egg-info")
 
-with open("README_python_glass.rst", "r", encoding="utf-8") as in_file:
+with open("README_glass_engine.rst", "r", encoding="utf-8") as in_file:
     content = in_file.read()
 
 with open("README.rst", "w", encoding="utf-8") as out_file:
     out_file.write(content)
 
-with open("setup_python_glass.py", "r", encoding="utf-8") as in_file:
+with open("setup_glass_engine.py", "r", encoding="utf-8") as in_file:
     content = in_file.read()
 
 with open("setup.py", "w", encoding="utf-8") as out_file:
@@ -23,13 +23,9 @@ with open("setup.py", "w", encoding="utf-8") as out_file:
 
 with open("MANIFEST.in", "w", encoding="utf-8") as out_file:
     out_file.write(
-"""include glass/README_PYPI.md
+"""include glass_engine/images/glass_engine_logo256.png
+include glass_engine/images/start.png
+include glass_engine/README_PYPI.md
 """)
 
 subprocess.call([sys.executable, "-m", "build", "--config-setting=-i", "--config-setting=https://pypi.tuna.tsinghua.edu.cn/simple"])
-
-with open("README_GlassEngine.rst", "r", encoding="utf-8") as in_file:
-    content = in_file.read()
-
-with open("README.rst", "w", encoding="utf-8") as out_file:
-    out_file.write(content)
