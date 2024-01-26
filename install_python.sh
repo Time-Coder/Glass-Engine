e8710acc6af5f7a124181c2a08182b2c2f003c93
Time-Coder <binghui.wang@foxmail.com>
2024-01-26 11:10:56 +0800

    update install_python script

--- install_python.sh
+++ install_python.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo apt -y install libssl-dev bzip2 libreadline-dev sqlite3 libbz2-dev
+sudo apt -y install libssl-dev libreadline-dev libbz2-dev liblzma-dev libsqlite3-dev
 
 python_versions=("3.7.9" "3.8.10" "3.9.13" "3.10.11" "3.11.7" "3.12.1")
 for version in ${python_versions[@]};
