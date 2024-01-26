1c023cf45ed83e62f07ca48017eacdd04378d5cb
Time-Coder <binghui.wang@foxmail.com>
2024-01-26 10:18:35 +0800

    complete build_assimpy.sh

--- build_assimpy.sh
+++ build_assimpy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+set -e
+
 python_versions=("3.7.9" "3.8.10" "3.9.13" "3.10.11" "3.11.7" "3.12.1")
 for version in ${python_versions[@]};
 do
-    echo $value;
+    pyenv install $version
+    pyenv local $version
+    python build_assimpy.py
 done;
