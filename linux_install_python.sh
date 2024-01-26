1fd2a47734f78c1c4ca70c63738ea9a73f4a063e
Time-Coder <binghui.wang@foxmail.com>
2024-01-26 17:52:16 +0000

    move assimp lib path

--- linux_install_python.sh
+++ linux_install_python.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-sudo apt -y install libssl-dev libreadline-dev libbz2-dev liblzma-dev libsqlite3-dev tk-dev patchelf libffi-dev
+# sudo apt -y install libssl-dev libreadline-dev libbz2-dev liblzma-dev libsqlite3-dev tk-dev patchelf libffi-dev
 
 python_versions=("3.7.9" "3.8.10" "3.9.13" "3.10.11" "3.11.7" "3.12.1")
 for version in ${python_versions[@]};
 do
-    wget --no-check-certificate https://npm.taobao.org/mirrors/python/$version/Python-$version.tar.xz -P ~/.pyenv/cache/
-    pyenv install $version
+    # wget --no-check-certificate https://npm.taobao.org/mirrors/python/$version/Python-$version.tar.xz -P ~/.pyenv/cache/
+    # pyenv install $version
     pyenv local $version
     $(pyenv which python) -m pip install pip --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple
     $(pyenv which python) -m pip install -r requirements.txt --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple
