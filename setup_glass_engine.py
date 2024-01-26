c0675d04916a565dd493ce2997308edafd695b8a
Time-Coder <binghuiwang5@gmail.com>
2024-01-27 01:33:10 +0800

    version 0.1.50 released

--- setup_glass_engine.py
+++ setup_glass_engine.py
@@ -22,7 +22,7 @@ extra_files += find_files("glass_engine", "glass_engine/glsl")
 
 setuptools.setup(
     name="glass_engine",
-    version="0.1.49",
+    version="0.1.50",
     author="王炳辉 (BingHui-WANG)",
     author_email="binghui.wang@foxmail.com",
     description="An easy-to-use 3D rendering engine for Python",
