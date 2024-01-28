b68d628c0fc4855d43f80153a2f3dc30b8075e69
Time-Coder <binghui.wang@foxmail.com>
2024-01-28 16:55:08 +0800

    remove trash files

--- show_normal.glsl
+++
@@ -1,9 +0,0 @@
-vec4 post_process(sampler2D screen_image, vec2 tex_coord)
-{
-    vec3 normal = world_normal_of(tex_coord);
-
-    vec4 color;
-    color.rgb = (normal + 1.0)/2.0;
-    color.a = 1;
-    return color;
-}
