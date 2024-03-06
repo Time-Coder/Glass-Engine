8f693358b1c4e2357f95047665fd32584afc1850
BingHui-WANG <binghui.wang@foxmail.com>
2024-03-06 14:10:35 +0800

    temp commit

--- glass/UniformVar.py
+++ glass/UniformVar.py
@@ -1,9 +1,57 @@
 from .utils import di
 from .ShaderParser import Var
 from .GlassConfig import GlassConfig
+from .GLInfo import GLInfo
 
 from OpenGL import GL
-from typing import Union, Dict
+from typing import Union, Dict, Any
+
+def setter(self, name:Union[int,str], value:Any):
+    if isinstance(name, str):
+        old_value = getattr(self, name)
+    else:
+        old_value = self[name]
+
+    if old_value == value:
+        return
+    
+    id_normal_var = id(self)
+    id_normal_cls = id(self.__class__)
+    old_setter = UniformVar._old_methods[id_normal_cls]["setter"]
+
+    if id_normal_var not in UniformVar._bound_vars:
+        old_setter(self, name, value)
+        return
+    
+    for uniform_var in UniformVar._bound_vars[id_normal_var]:
+        for key, child_var in uniform_var._var.children.items():
+            if child_var.type in GLInfo.atom_type_map:
+                if isinstance(key, str):
+                    setattr(uniform_var, key, getattr(value, key))
+                elif isinstance(key, int):
+                    uniform_var[key] = value[key]
+            else:
+                if isinstance(key, str):
+                    attr = getattr(value, key)
+                elif isinstance(key, int):
+                    attr = value[key]
+                    
+                attr_cls = attr.__class__
+                id_attr_cls = id(attr_cls)
+                if id_attr_cls not in UniformVar._old_methods:
+                    if "[" not in child_var.type:
+                        if hasattr(attr_cls, "__setattr__"):
+                            UniformVar._old_methods[id_attr_cls]["__setattr__"] = attr_cls.__setattr__
+
+                        attr_cls.__setattr__ = setter
+                    else:
+                        if hasattr(attr_cls, "__setitem__"):
+                            UniformVar._old_methods[id_attr_cls]["__setitem__"] = attr_cls.__setitem__
+
+                        attr_cls.__setitem__ = setter
+
+    
+                
 
 class UniformVar:
     _all_attrs = {
@@ -24,9 +72,13 @@ class UniformVar:
         "location",
     }
 
+    _bound_vars = {}
+    _old_methods = {}
+
     def __init__(self, uniform_id:int, var:Var):
         self._uniform_id:int = uniform_id
         self._var:Var = var
+        self._normal_var:Any = None
         self._children:Dict[Union[str,int], UniformVar] = {}
 
     @property
@@ -106,3 +158,27 @@ class UniformVar:
             return super().__setattr__(name, value)
 
         self[name] = value
+
+    def bind(self, normal_var):
+        if self._normal_var is not None and self._normal_var is not normal_var:
+            self.unbind()
+
+        if self._var.type in GLInfo.atom_type_map:
+            raise TypeError(f"cannot bind to primitive type '{self._var.type}'")
+        
+        id_normal_var = id(normal_var)
+        if id_normal_var not in UniformVar._bound_vars:
+            UniformVar._bound_vars[id_normal_var] = set()
+            normal_cls = normal_var.__class__
+            id_normal_cls = id(normal_var.__class__)
+            if id_normal_cls not in UniformVar._old_methods:
+                UniformVar._old_methods[id_normal_cls] = {}
+
+                if hasattr(normal_var, "__setattr__"):
+                    UniformVar._old_methods[id_normal_cls]["__setattr__"] = normal_cls.__setattr__
+
+                if hasattr(normal_var, "__setitem__"):
+                    UniformVar._old_methods[id_normal_cls]["__setitem__"] = normal_cls.__setitem__
+
+        if self not in UniformVar._bound_vars[id_normal_var]:
+            UniformVar._bound_vars[id_normal_var].add(self)
